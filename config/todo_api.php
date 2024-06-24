405f2ecc5ccc195c2128c8d1778e5e137ab3a564
Daniel <boyelanrewaju@gmail.com>
2024-06-24 22:02:43 +0100

    project setup

---
+++ config/todo_api.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application URL
+    |--------------------------------------------------------------------------
+    |
+    | This value is the Todo application url, which will be used when the
+    | for all requests to the Todo API.
+    |
+    */
+
+    'url' => env('TODO_API_URL', 'http://localhost'),
+];
